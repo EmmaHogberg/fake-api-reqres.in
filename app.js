087b9b3a53cea9b29dc40a06aac27668195f18e2
EmmaHogberg <70634183+EmmaHogberg@users.noreply.github.com>
2020-10-22 17:19:35 +0200

    Finished and clean

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+// Hämta API-url och html-element
 const apiUrl = "https://reqres.in/";
 const formEl = document.querySelector("#loginForm");
 const showUserButtonEl = document.querySelector(".showUsersButton");
 const usersListEl = document.querySelector(".usersList");
 const userInfoContainer = document.querySelector(".userInfoContainer");
 
+// Lyssna efter klick på "logga in"-knappen
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  // Fetcha inloggning
   fetch(apiUrl + "api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -17,16 +20,19 @@ formEl.addEventListener("submit", (event) => {
   })
     .then((res) => res.json())
     .then((jsonData) => {
+      // Error-meddelande vid fel inloggningsuppgifter
       if (jsonData.error) {
         const errorMessageEl = document.querySelector("#loginErrorMessage");
-        errorMessageEl.innerText = jsonData.error;
-        errorMessageEl.classList.remove("hide");
-        errorMessageEl.classList.remove;
-      } else showUserButtonEl.classList.remove("hide");
+        errorMessageEl.innerText = "Error: " + jsonData.error;
+      }
+      // Visa "show users"-knappen
+      else showUserButtonEl.hidden = false;
     });
 });
 
+// Lyssna efter klick på "show users"-knappen
 showUserButtonEl.addEventListener("click", (e) => {
+  // Fetcha användaruppgifter och skapa lista med användare
   fetch(apiUrl + "api/users")
     .then((res) => res.json())
     .then((data) => {
@@ -40,10 +46,11 @@ showUserButtonEl.addEventListener("click", (e) => {
     });
 });
 
+// Lyssna efter klick på enskild användares namn
 usersListEl.addEventListener("click", (e) => {
   const userId = e.target.dataset.userid;
 
-  // Fetch singel user
+  // Fetcha enskild användares info
   fetch(`${apiUrl}api/users/${userId}`)
     .then((res) => res.json())
     .then((user) => {
